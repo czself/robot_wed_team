58bf85f1230ee28372231b3dd13478c686939e6d
czself <3421798408@qq.com>
2026-08-12 23:41:09 +0800

    Show robots on home page

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Hero from "@/components/Hero";
 import About from "@/components/About";
 import SystemStatus from "@/components/SystemStatus";
 import TeamShowcase from "@/components/TeamShowcase";
+import Robots from "@/components/Robots";
 import WhyJoin from "@/components/WhyJoin";
 import RecruitProcess from "@/components/RecruitProcess";
 import Join from "@/components/Join";
@@ -14,6 +15,7 @@ export default function Home() {
         <About />
         <SystemStatus />
         <TeamShowcase />
+        <Robots />
         <WhyJoin />
         <RecruitProcess />
         <Join />
